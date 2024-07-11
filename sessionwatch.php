25e8d359ddfdba16ab57c4b2956e334da7afa715
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2024-07-11 23:52:04 +0200

    First Part of Session Watch

---
+++ sessionwatch.php
@@ -0,0 +1,53 @@
+<?php
+
+class sessionwatch {
+
+    public static function setup():void {
+        $path = DOCROOT.'/data/session';
+        if(!file_exists($path)){
+            if(!mkdir($path) && !is_dir($path)){
+                throw new \Exception('Der Pfad konnte nicht angelegt werden.');
+            }
+        } else {
+            throw new \Exception('Der Pfad konnte nicht angelegt werden.');
+        }
+
+    }
+
+    public static function write(?string $param = null):void{
+        $data['session'] = session_id();
+        $data['class'] = debug_backtrace()[1]['class'];
+        $data['function'] = debug_backtrace()[1]['function'];
+        if(!is_null($param)){
+        $data['param'] = $param;
+        }
+
+        $user = NULL;
+        if(isset($_SESSION['username'])){
+            $user = $_SESSION['username'];
+        }
+        self::log($data,$user);
+    }
+
+
+    public static function log(array $data,?string $user = NULL):void{
+        $filename = DOCROOT.'/data/session/'.$user.'_'.$data['session'].'.json';
+        $json = json_encode($data);
+        $str = json_decode($json,true);
+        if(($temp = file_get_contents($filename)) != false){
+            $tempArray = json_decode($temp,true);
+        }
+        array_push($tempArray,$str);
+    }
+
+
+    public static function read():?array{
+
+}
+
+    public static function files():?array{
+
+    }
+
+
+}
